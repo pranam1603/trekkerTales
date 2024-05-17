de99bfed3afff8361a90b649620aeef629b1757f
knptravels <knptravelsbpl@gmail.com>
2024-05-18 00:42:53 +0530

    17may

--- schema.js
+++ schema.js
@@ -1,9 +1,14 @@
 const Joi = require("joi");
 
-module.exports.joiSchema = Joi.object({
+module.exports.joiTreksitesSchema = Joi.object({
     title: Joi.string().required(),
     location: Joi.string().required(),
     image: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().required()
+}).required();
+
+module.exports.joiReviewSchema = Joi.object({
+    rating: Joi.number().required(),
+    review: Joi.string().required()
 }).required();
